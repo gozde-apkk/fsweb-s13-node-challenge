8af66e8c6dc850b499743cad197f7d0c1c1356d5
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-28 17:53:09 +0300

    fixed

--- api/server.js
+++ api/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const server = express();
 
 // Sunucunuzu yapılandırın
+
+server.use(express.json());
+const actionRouter = require("./actions/actions-router");
+const projectRouter=require("./projects/projects-router");
+
+
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
 // Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
 // Bu dosyanın içinde `server.listen()` YAPMAYIN!
-
+server.get("/", (req,res) =>{
+    res.send("Node App working on port ...")
+}
+)
 module.exports = server;
