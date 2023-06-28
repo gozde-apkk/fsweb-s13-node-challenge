c9c9eafeea4deece30fd87065b7a89c25a7f6605
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-28 17:51:55 +0300

    project-middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,37 @@
 // projects ara yazılımları buraya
+
+
+const projectModel = require("../projects/projects-model");
+
+async function validateUserId(req,res,next) {
+    try{
+        let existUser = await projectModel.get(req.params.id);
+        if(!existUser){
+            res.status(404).json({
+                message:"Proje bulunamadı....."
+            })
+        }else{
+            req.existUser = existUser;
+        }
+    }catch(error){
+        next(error);
+    }
+}
+
+
+function validatePayload (req , res, next){
+    try{
+        let {name, description} = req.body;
+        if(!name || !description){
+            res.status(400).json({message:"Eksik alanları kontrol ediniz"});
+        }else{
+            next();
+        }
+    }catch(error){
+        next(error);
+    }
+}
+
+
+module.exports = {validatePayload,validateUserId};
+
